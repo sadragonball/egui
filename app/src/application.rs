829c4d91e5a0951fb0b36b18f19838e22c8ff1f9
Xuefujun <meidofuku@hotmail.com>
2022-08-10 09:59:40 +0800

    tri render

--- app/src/application.rs
+++ app/src/application.rs
@@ -14,7 +14,7 @@ pub struct State {
 pub struct Application {
     state: State,
     //modules
-
+    volume_renderer: crate::modules::VolumeRenderer,
     //files
     dropped_files: Vec<egui::DroppedFile>,
 }
@@ -23,6 +23,7 @@ impl Application {
     pub fn  new(cc: &eframe::CreationContext<'_>) -> Self {
         let mut app = Self {
             state: State::default(),
+            volume_renderer: crate::modules::VolumeRenderer::new(cc),
             dropped_files: Default::default()
         };
 
@@ -34,11 +35,11 @@ impl Application {
 
         app
     }
-//    fn  modules_iter_mut(&mut self) -> impl Iterator<Item = (&str, &str, &mut dyn eframe::App)>
-    fn  modules_iter_mut(&mut self) -> impl Iterator<Item = (&str, &str)> {
+    fn  modules_iter_mut(&mut self) -> impl Iterator<Item = (&str, &str, &mut dyn eframe::App)> {
         let mut vec = vec![(
             "yingtan volume renderer",
-            "volume renderer"
+            "volume_renderer",
+            &mut self.volume_renderer as &mut dyn eframe::App
             )];
 
         vec.into_iter()
@@ -59,6 +60,8 @@ impl App for Application {
                 self.bar_contents(ui, frame);
             })
         });
+
+        self.show_selected_module(ctx, frame);
     }
 
     #[cfg(feature = "persistence")]
@@ -75,6 +78,16 @@ impl App for Application {
 
 //UI part
 impl Application {
+    fn show_selected_module(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        let mut found_anchor = false;
+        let selected_anchor = self.state.selected_anchor.to_owned();
+        for (_name, _anchor, _module) in self.modules_iter_mut() {
+            if _anchor == selected_anchor || ctx.memory().everything_is_visible() {
+                _module.update(ctx, frame);
+                found_anchor = true;
+            }
+        }
+    }
     fn bar_contents(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
         egui::widgets::global_dark_light_mode_switch(ui);
 
@@ -82,6 +95,15 @@ impl Application {
 
         ui.label("hello, yingtan volume renderer rs");
 
+        ui.separator();
+
+        let mut selected_anchor = self.state.selected_anchor.to_owned();
+        for (name, anchor,_module) in self.modules_iter_mut() {
+            if ui.selectable_label(selected_anchor == anchor, name).clicked() {
+                selected_anchor = anchor.to_owned();
+            }
+        }
+        self.state.selected_anchor = selected_anchor;
         egui::warn_if_debug_build(ui);
     }
 }
